3d6f5a18df400a4cacf8ddb5ac228234635d0407
mallory13 <mallory.steele13@gmail.com>
2016-04-21 10:40:24 -0400

    brackets

--- Scripts/app.js
+++ Scripts/app.js
@@ -8,12 +8,12 @@
     //generate random numbers between 1 - 6
     $('#rollButton').click(function() {
         var dieNum = (Math.floor(Math.random() * 6) + 1);
-        $('#die1Image').attr('src', 'Content/images/die' + dieNum + '.png');
+        $('#die1Image').attr('src', ('Content/images/die' + dieNum + '.png'));
         $('#die1Paragraph').val(dieNum);
         
         
         var dieNum2 = (Math.floor(Math.random() * 6) + 1);
-        $('#die2Image').attr('src', 'Content/images/die' + dieNum + '.png');
+        $('#die2Image').attr('src', ('Content/images/die' + dieNum + '.png'));
         $('#die2Paragraph').val(dieNum2);
         
         
