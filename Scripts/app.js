c81a54475f05171fce8eaea3de31e21d68ec365d
mallory13 <mallory.steele13@gmail.com>
2016-04-21 10:59:19 -0400

    test

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,22 +4,26 @@
 (function() {
 
     "use strict";
+    
+    $( "#rollButton" ).click(function() {
+  alert( "Handler for .click() called." );
+});
 
     //generate random numbers between 1 - 6
-    $('#rollButton').click(function() {
-        //console.log("works");
+    // $('#rollButton').click(function() {
+    //     //console.log("works");
         
-        var dieNum = (Math.floor(Math.random() * 6) + 1);
-        $('#die1Image').attr('src', ('Content/images/die' + dieNum + '.png'));
-        $('#die1Paragraph').val(dieNum);
+    //     var dieNum = (Math.floor(Math.random() * 6) + 1);
+    //     $('#die1Image').attr('src', ('Content/images/die' + dieNum + '.png'));
+    //     $('#die1Paragraph').val(dieNum);
         
         
-        var dieNum2 = (Math.floor(Math.random() * 6) + 1);
-        $('#die2Image').attr('src', ('Content/images/die' + dieNum + '.png'));
-        $('#die2Paragraph').val(dieNum2);
+    //     var dieNum2 = (Math.floor(Math.random() * 6) + 1);
+    //     $('#die2Image').attr('src', ('Content/images/die' + dieNum + '.png'));
+    //     $('#die2Paragraph').val(dieNum2);
         
 
-    });
+    // });
 
 
 
