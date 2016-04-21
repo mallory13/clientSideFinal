05538cace64c9ec867b97765e319b89cdbf4bc4a
mallory13 <mallory.steele13@gmail.com>
2016-04-21 11:03:18 -0400

    test

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,27 +3,28 @@
 // setup your IIFE (Immediately Invoked Function Expression)
 (function() {
 
-    "use strict";
+    //"use strict";
     
-    $( "button" ).click(function() {
-  alert( "Handler for .click() called." );
-});
+    //test button
+//     $( "button" ).click(function() {
+//   alert( "Handler for .click() called." );
+// });
 
     //generate random numbers between 1 - 6
-    // $('#rollButton').click(function() {
-    //     //console.log("works");
+    $('#rollButton').click(function() {
+        //console.log("works");
         
-    //     var dieNum = (Math.floor(Math.random() * 6) + 1);
-    //     $('#die1Image').attr('src', ('Content/images/die' + dieNum + '.png'));
-    //     $('#die1Paragraph').val(dieNum);
+        var dieNum = (Math.floor(Math.random() * 6) + 1);
+        $('#die1Image').attr('src', ('Content/images/die' + dieNum + '.png'));
+        $('#die1Paragraph').val(dieNum);
         
         
-    //     var dieNum2 = (Math.floor(Math.random() * 6) + 1);
-    //     $('#die2Image').attr('src', ('Content/images/die' + dieNum + '.png'));
-    //     $('#die2Paragraph').val(dieNum2);
+        var dieNum2 = (Math.floor(Math.random() * 6) + 1);
+        $('#die2Image').attr('src', ('Content/images/die' + dieNum + '.png'));
+        $('#die2Paragraph').val(dieNum2);
         
 
-    // });
+    });
 
 
 
